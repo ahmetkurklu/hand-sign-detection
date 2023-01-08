b4153a5e180f8bce9634f09526449e07fd6ca252
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-08 11:52:04 +0100

    Ajout du rectangle dans l'image

--- Capture.py
+++ Capture.py
@@ -9,12 +9,22 @@ capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
+
+# coordonnées du rectangle a changé en fonction du label
+x1 = 0 + 500
+y1 = 0 + 250
+x2 = 1280 - 500
+y2 = 720 - 200
+
 while True:
     #capture d'une image du flux de la webcam
     ret,img = capture.read()
     if not ret:
         print("Erreur lors de la lecture de img")
         break
+
+    # Trace le rectangle sur l'image
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
     #Affichage de l'image
     cv2.imshow("Image", img)
@@ -26,9 +36,10 @@ while True:
         break
     # ESPACE
     elif key%256 == 32:
-        #Ecrit l'image dans le fichier
+        #Ecrit le roi dans le fichier
+        roi = img[y1+2:y2-1, x1+2:x2-1]
         img_name = "image\{0}\{0}_{1}.png".format(label,cnt_img)
-        cv2.imwrite(img_name, img)
+        cv2.imwrite(img_name, roi)
         print("{} ecrit!".format(img_name))
         cnt_img += 1
 
